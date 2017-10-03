446044f22ee9bc8488f4aafde93d6b825e2eea61
Marcin Dąbrowski <marcin.dabrowski@lingaro.com>
2017-10-04 01:41:33 +0200

    Set server IP Address to '0.0.0.0' to work on Heroku.

--- src/server/index.js
+++ src/server/index.js
@@ -6,10 +6,10 @@ const initServer = require('./initServer');
 const app = express();
 const DOCS_PATH = '../../docs/';
 const PORT = process.env.PORT || 8082;
-const IP_ADRESS = 'localhost';
+const IP_ADDRESS = '0.0.0.0';
 
 app.set('port', PORT);
-app.set('ipAdress', IP_ADRESS);
+app.set('ipAddress', IP_ADDRESS);
 
 app.use(express.static(path.join(__dirname, DOCS_PATH)));
 
@@ -18,12 +18,12 @@ initServer(app);
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, DOCS_PATH, 'index.html')));
 
 /* eslint-disable no-console */
-app.listen(
-  PORT,
-  IP_ADRESS,
-  () => console.log(`
+app.listen(PORT, IP_ADDRESS, () =>
+  console.log(`
     =====================================================
-    -> Server (${chalk.bgBlue('SPA')}) 🏃 (running) on ${chalk.green(IP_ADRESS)}:${chalk.green(PORT)}
+    -> Server (${chalk.bgBlue('SPA')}) 🏃 (running) on ${chalk.green(IP_ADDRESS)}:${chalk.green(
+  PORT,
+)}
     =====================================================
   `),
 );
